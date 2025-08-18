68bfd5bac4266d9349ff45524e29424ad8796a28
Vicente10- <140712692+vicentepr@users.noreply.github.com>
2025-08-17 21:13:11 -0300

    Update ProdutoRepository.java

--- ProdutoRepository.java
+++ ProdutoRepository.java
@@ -9,13 +9,11 @@ import java.util.List;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 
-    // Busca por parte do nome
     List<Produto> findByNomeIgnoreCaseContaining(String nome);
 
-    // Busca por faixa de preço
     List<Produto> findByPrecoBetween(BigDecimal precoMin, BigDecimal precoMax);
 
-    // Consulta JPQL para estoque baixo
     @Query("SELECT p FROM Produto p WHERE p.estoque < :quantidade")
     List<Produto> produtosComEstoqueBaixo(Integer quantidade);
 }
+
